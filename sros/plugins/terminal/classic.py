8af707a58696b06ebb2986c4c847454376b3cfd2
wiso <wisotzky@users.noreply.github.com>
2020-05-04 13:23:12 +0200

    fix for added for initial CLI prompt detection, if motd is disabled

--- sros/plugins/terminal/classic.py
+++ sros/plugins/terminal/classic.py
@@ -22,8 +22,8 @@ from ansible.module_utils._text import to_text
 
 class TerminalModule(TerminalBase):
     terminal_stdout_re = [
-        re.compile(br"[\r\n]+\!?\*?(\((ex|gl|pr|ro)\))?\[.*\][\r\n]+[ABCD]\:\S+\@\S+\#\s"),
-        re.compile(br"[\r\n]+\*?[ABCD]:[\w\-\.\>]+[#\$]\s")
+        re.compile(br"[\r\n]*\!?\*?(\((ex|gl|pr|ro)\))?\[.*\][\r\n]+[ABCD]\:\S+\@\S+\#\s"),
+        re.compile(br"[\r\n]*\*?[ABCD]:[\w\-\.\>]+[#\$]\s")
     ]
 
     terminal_stderr_re = [
